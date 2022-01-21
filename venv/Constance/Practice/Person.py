14582f9ebd58a4254628a60ccd71bc14a80baa33
Dean <deaneryfb@gmail.com>
2022-01-21 15:05:13 -0600

    practice part 2, using classmethod

--- venv/Constance/Practice/Person.py
+++ venv/Constance/Practice/Person.py
@@ -1,9 +1,13 @@
 class Person:
     person_counter = 0 # this will be share with every object
+    @classmethod
+    def generate_netx_value(cls):  # REMEMBER: with this (cls) we access to our class variables
+        cls.person_counter += 1
+        return cls.person_counter
 
     def __init__(self, name, age):
-        Person.person_counter += 1
-        self.id_person = Person.person_counter
+        #Person.person_counter += 1
+        self.id_person = Person.generate_netx_value()
         self.name = name
         self.age = age
 
