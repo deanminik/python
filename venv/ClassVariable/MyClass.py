b72f96168ad3e091f5b7a5908fa57ce8ce1883c0
Dean <deaneryfb@gmail.com>
2022-01-21 11:54:50 -0600

    class methods

--- venv/ClassVariable/MyClass.py
+++ venv/ClassVariable/MyClass.py
@@ -5,6 +5,18 @@ class MyClass:
     def __init__(self, instance_variable):
         self.instance_variable = instance_variable
 
+    #creating a static method
+    @staticmethod # remember, a decorator allows to affect the next method to associate the class by itself and not with the objects
+    def static_method():
+        print(MyClass.variable_class)
+    #creating a class method
+    @classmethod
+    def class_method(cls): #we receive this parameter cls = class from classmethod
+        print(cls.variable_class)
+
+MyClass.class_method()
+#MyClass.static_method()
+"""
 print(MyClass.variable_class)
 myclass = MyClass('Value variable instance')
 print(myclass.instance_variable)
@@ -17,4 +29,4 @@ print(myclass2.variable_class)
 #this is a dynamic variable
 MyClass.variable_class2 = 'Value variable class 2'
 print(MyClass.variable_class2)
-
+"""
