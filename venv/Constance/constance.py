ab0a23b91052823403fa0f3ec35c1ba22e17164f
Dean <deaneryfb@gmail.com>
2022-01-21 14:38:56 -0600

    Constances part 2

--- venv/Constance/constance.py
+++ venv/Constance/constance.py
@@ -8,3 +8,9 @@ There is a recommendation to use the variables in a different module
 #1- To use the convention, it is a required to write with uppercase the variable
 MY_CONSTANCE = 'The value of my constance'
 
+class Mathematics:
+    PI = '3.1416' #REMEMBER: This is a variable of class because it is outside the any method
+    #And it is associated with the class and not with the objects
+    #We can access to this variable from another module just calling the class. It is not necessary
+    #to build an object to access to this variable PI
+
--- venv/Constance/test_constance.py
+++ venv/Constance/test_constance.py
@@ -1,5 +1,10 @@
+#from constance import * # (*) the meaning  is to import the all modules
 from constance import MY_CONSTANCE
+from constance import Mathematics as Math # (math is an alias )
+
 
 print(MY_CONSTANCE)
 MY_CONSTANCE = 'New value'
 print(MY_CONSTANCE)
+#print(Mathematics.PI)
+print(Math.PI)
