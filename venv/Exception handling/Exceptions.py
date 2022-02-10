a7f867fa5f56dfd6e38e06704999956774c282cf
Dean <deaneryfb@gmail.com>
2022-02-09 20:33:34 -0600

    try and catch errors

---
+++ venv/Exception handling/Exceptions.py
@@ -0,0 +1,56 @@
+# example 1
+print('Example 1')
+# 10/0
+
+"""
+Traceback (most recent call last):
+  File "/home/lite/PycharmProject/LabGeometric/venv/Exception handling/Exceptions.py", line 1, in <module>
+    10/0
+ZeroDivisionError: division by zero
+"""
+
+try:
+    10/0
+except Exception as e:
+    print(f'''
+     We found the next error saved here (e)
+     {e}
+''')
+
+"""
+  We found the next error saved here (e)
+     division by zero
+
+"""
+
+print('Example 2')
+
+# example 2
+result = None # none means that there is an assign value
+a = 10
+b = 0
+
+try:
+    result = a/b
+except Exception as e:
+# except ZeroDivisionError as e: this is more specific
+    #print(f'There is an error:{e}')
+    print(f'There is an error:{e}, {type(e)}')
+
+print(f'Result {result}')
+
+
+# example 3
+# using else and finally
+print('Example 3')
+result2 = None
+try:
+    a = int(input('add the first number: '))
+    b = int(input('add the second number: '))
+    result2 = a/b
+except Exception as e:
+    print(f'There is an error : {e}, {type(e)}')
+else:
+    print('There is not exception :)') # this is when the code works correctly
+finally:
+    print('This (finally) executes whatever there is an error or not ')
---
+++ venv/Exception handling/description.md
@@ -0,0 +1,9 @@
+Python has an important methods to use when we detect
+errors 
+
+see this image to see the hierarchy
+* Aritmetic errors -> division with cero error
+* System operator errors
+* When the software is running -> runtimerrrors
+* Syntax in the code -> SyntaxError 
+* and there are children classes from them 
