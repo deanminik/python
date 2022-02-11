c441f495921ba206e201f602bb1b2039ab620a78
Dean <deaneryfb@gmail.com>
2022-02-11 15:58:38 -0600

    using (with) instead connection.autocommit and connection.commit() -> this is the correct

--- venv/PostgresqlDatabase/transactions_commit_rollback.py
+++ venv/PostgresqlDatabase/transactions_commit_rollback.py
@@ -10,14 +10,12 @@ connection = bd.connect(
 try:
    with connection:
        with connection.cursor() as cursor:
-           connection.autocommit = False # this is to don't save the changes automatically
-           cursor = connection.cursor()
            sentence = 'INSERT INTO person(per_name, per_lastname, per_email) VALUES (%s, %s, %s)'
            values = ('Mari++++', 'Barker', 'mymail@gmail.com')
            cursor.execute(sentence, values)
 
-           sentence = 'UPDATE person SET per_name=%s, per_lastname, per_email WHERE id_person=%s'
-           values = ('Juank','rodriguez','hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',1)
+           sentence = 'UPDATE person SET per_name=%s, per_lastname=%s, per_email%s WHERE id_person=%s'
+           values = ('Juank','rodriguez','hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',1)
            cursor.execute(sentence, values)
 
 except Exception as e:
