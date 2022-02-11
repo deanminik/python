7a27af8ae642df15b96e905b00a9d85f9fdea627
Dean <deaneryfb@gmail.com>
2022-02-10 21:54:48 -0600

    movie catalog done with the methods add, read, and delete from os class

---
+++ movie catalog/service/movie_catalog.py
@@ -0,0 +1,25 @@
+import os
+
+# os operating system has the method delete
+
+class MovieCatalog:
+    variable_path = 'movies.txt'
+
+    # we can use a static method or class method in this case we are going to use a class method because we have a
+    # class variable and static method does not have a context class
+    @classmethod
+    # but we can access directly to our class variable (variable_path )
+    def add_movie(cls, movie):
+        with open(cls.variable_path, 'a') as file:  # 'a' -> append method
+            file.write(f'{movie.name}\n')
+
+    @classmethod
+    def listing_movies(cls):
+        with open(cls.variable_path, 'r') as file:  # 'r' -> only read
+            print('Movie catalog'.center(50, '-'))
+            print(file.read())
+
+    @classmethod
+    def delete_movies(cls):
+        os.remove(cls.variable_path)
+        print(f'file deleted: {cls.variable_path}')
