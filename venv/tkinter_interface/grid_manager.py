9dcdb5eae4122615d7f02d58ded86b7cce8c1f93
Dean <deaneryfb@gmail.com>
2022-03-20 16:23:42 -0600

    Grid manager positions

--- venv/tkinter_interface/grid_manager.py
+++ venv/tkinter_interface/grid_manager.py
@@ -6,6 +6,17 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Grid Manager')
 
+# grid configuration adding lines sizes
+# first row
+window.rowconfigure(0, weight=1)
+# second row
+window.rowconfigure(1, weight=10)
+# first column
+window.columnconfigure(0, weight=1)
+# second column
+window.columnconfigure(1, weight=5)
+
+
 # define events methods
 def event1():
     btn1.config(text='Pressed button')
@@ -15,9 +26,23 @@ def event2():
 #define 2 btn
 btn1 = ttk.Button(window, text='Button 1', command=event1)
 #btn1.pack()
-btn1.grid(row=0, column=0)
+#btn1.grid(row=0, column=0)
+btn1.grid(row=0, column=0, sticky='NSWE') # NSWE -> get the all space available
 
+"""
+to work with sticky 
+N -> up
+E -> right
+S -> down
+W -> left
+"""
 
 btn2 = ttk.Button(window, text='Button 2', command=event2)
-btn2.grid(row=1, column=0)
+btn2.grid(row=1, column=0, sticky='NSWE')
+
+btn3 = ttk.Button(window, text='Button 3')
+btn3.grid(row=0, column=1, sticky='NSWE')
+
+btn4 = ttk.Button(window, text='Button 4')
+btn4.grid(row=1, column=1, sticky='NSWE')
 window.mainloop()
