52890c3176dff6e4d10a8585e3c36f847152a2fc
Dean <deaneryfb@gmail.com>
2022-03-20 20:18:43 -0600

    Tkinter labels

--- venv/tkinter_interface/entryComponent.py
+++ venv/tkinter_interface/entryComponent.py
@@ -1,10 +1,9 @@
 import tkinter as tk
-# import the module of the tkinder theme
 from tkinter import ttk
 
 window = tk.Tk()
 window.geometry('600x400')
-window.title('Entry components')
+window.title('handle components')
 
 
 window.rowconfigure(0, weight=1)
@@ -16,22 +15,14 @@ window.columnconfigure(1, weight=5)
 entry_var1 = tk.StringVar(value='Value by default')
 entry1 = ttk.Entry(window, width=30 , textvariable=entry_var1)
 entry1.grid(row=0,column=0)
-# insert
-#entry1.insert(0,'Add a text')
-#entry1.insert(tk.END, '.') # add at the end this tex
-#entry1.config(state='readonly')
+#labels
+label1 = tk.Label(window, text='Here will show the content of the box')
+label1.grid(row=1, column=0, columnspan=2) # columnspan=2 -> use two column
 
 
 # Create a btn to get the data from the entry
 def send():
-    # recover the data from variable
-    btn1.config(text=entry_var1.get())
-    # modify with set
-    entry_var1.set('Changing...')
-    # recover
-    print(entry_var1.get())
-    print(entry1.get())
-
+    label1.config(text=entry_var1.get())
 
 
 btn1 = ttk.Button(window, text='SEND', command=send)
